6e5433b3ab005f92ec8e4d20dbcd51fc19c26e55
Nikhil Singh <nikhilsingh@Nikhils-MacBook-Pro.local>
2019-12-21 16:35:59 -0500

    4 endpoints done

---
+++ project1-ui/src/action-mappers/login-action-mappers.ts
@@ -0,0 +1,45 @@
+import { userLogin } from "../remote/reimbursements-clients/reimbursements-user"
+
+
+// we will make an object with constant values
+// so we dont have to worry about spelling
+export const uLoginTypes = {
+    INVALID_CREDENTIALS: 'USER_LOGIN_INVALID_CREDENTIALS',
+    SUCCESSFUL_LOGIN: 'USER_LOGIN_SUCCESSFUL_LOGIN',
+    UNSUCCESSFUL_LOGIN: 'USER_LOGIN_FAILED_LOGIN'
+}
+
+// first action mapper
+//it is going to try and login
+// depending on what we get back, will will return a different action
+// this action will get sent to dispatch
+// kind of
+// this is tricky, because we are doing an async operation
+// when we do async, we return a function that returns an action
+//then, redux thunk is going to intercept that function, and call it when the async is done
+
+export const uLogin = (username:string, password:string) => async (dispatch:any) => {
+
+    try{
+        let res = await userLogin(username, password)
+        //a successful login
+        if(res.status === 200){
+            //this is how do it when we have async operations
+            dispatch({
+                type:uLoginTypes.SUCCESSFUL_LOGIN,
+                payload:{
+                    user:res.body
+                }
+            })
+        }else{
+            dispatch({
+                type:uLoginTypes.INVALID_CREDENTIALS
+            })
+        }
+    }catch(e){
+        dispatch({
+            type:uLoginTypes.UNSUCCESSFUL_LOGIN
+        })
+    }
+    
+}
